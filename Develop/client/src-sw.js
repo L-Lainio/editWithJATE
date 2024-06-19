3474a1baa01ebfeb9d0f0782ca40f776aefb5898
L-Lainio <arollainio@gmail.com>
2024-06-19 15:00:00 -0400

    corrected webpackservice worker and it went from 404 to 502 err code

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -4,6 +4,7 @@ const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+const { InjectManifest } = require('workbox-webpack-plugin');
 
 // The precacheAndRoute() method takes an array of URLs to precache. The self._WB_MANIFEST is an array that contains the list of URLs to precache.
 precacheAndRoute(self.__WB_MANIFEST);
@@ -27,8 +28,6 @@ warmStrategyCache({
 
 
 registerRoute(
-
-
   ({ request }) => request.mode === 'navigate', pageCache);
 
 // Set up asset cache
@@ -50,3 +49,35 @@ registerRoute(
     ],
   })
 );
+
+const nextConfig = {
+  webpack(config, options) {
+    if (!options.isServer) {
+      const workboxPlugin = new InjectManifest({
+        swSrc: "./src/service-worker/index.ts",
+        swDest: "../public/service-worker.js",
+        // In dev, exclude everything.
+        // This avoids irrelevant warnings about chunks being too large for caching.
+        // In non-dev, use the default `exclude` option, don't override.
+        ...(options.dev ? { exclude: [/./] } : undefined),
+      })
+      if (options.dev) {
+        // Suppress the "InjectManifest has been called multiple times" warning by reaching into
+        // the private properties of the plugin and making sure it never ends up in the state
+        // where it makes that warning.
+        // https://github.com/GoogleChrome/workbox/blob/v6/packages/workbox-webpack-plugin/src/inject-manifest.ts#L260-L282
+        Object.defineProperty(workboxPlugin, "alreadyCalled", {
+          get() {
+            return false
+          },
+          set() {
+            // do nothing; the internals try to set it to true, which then results in a warning
+            // on the next run of webpack.
+          },
+        })
+      }
+      config.plugins.push(workboxPlugin)
+    }
+    return config
+  }
+}
